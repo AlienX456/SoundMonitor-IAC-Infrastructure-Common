1b138a55bfb786e425cffda5a35f47a1ebe7a69c
alienx456 <estebanelias27dev@gmail.com>
2021-09-19 12:37:41 -0500

    get map file from current file

--- use-cases/ecs_s3_kafka_services/locals.tf
+++ use-cases/ecs_s3_kafka_services/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  topic-index-mapping-json = [for i in jsondecode(file("../../map/topic-index-mapping.json")) : i if i.process-type == var.process-type][0]
+  topic-index-mapping-json = [for i in jsondecode(file("topic-index-mapping.json")) : i if i.process-type == var.process-type][0]
   names = {
     service = "${var.repository_name}-service"
     family  = var.repository_name
