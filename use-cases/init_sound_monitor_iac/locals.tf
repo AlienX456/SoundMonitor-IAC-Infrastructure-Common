3b9d63d2b92d519abb3c3b9be0528233a26b6952
alienx456 <estebanelias27dev@gmail.com>
2021-09-26 14:51:44 -0500

    format, fix quotes

--- use-cases/init_sound_monitor_iac/locals.tf
+++ use-cases/init_sound_monitor_iac/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  topic-index-mapping-audio = [for i in jsondecode(file("../../map/topic-index-mapping.json")) : i if i.process-type == 'audio'][0]
-  topic-index-mapping-images = [for i in jsondecode(file("../../map/topic-index-mapping.json")) : i if i.process-type == 'images'][0]
+  topic-index-mapping-audio  = [for i in jsondecode(file("../../map/topic-index-mapping.json")) : i if i.process-type == "audio"][0]
+  topic-index-mapping-images = [for i in jsondecode(file("../../map/topic-index-mapping.json")) : i if i.process-type == "images"][0]
 }
